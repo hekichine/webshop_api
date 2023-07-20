5b596c0750260d0c1f31e297849895b0d5fc5fbf
hekichien <chienlx1005@gmail.com>
2023-07-20 13:43:28 +0700

    .

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,5 @@
 import express from "express";
-import http from "http";
 import bodyParser from "body-parser";
-import cookieParser from "cookie-parser";
-import compression from "compression";
 import cors from "cors";
 
 import connect from "./connectDB/connectDB";
@@ -10,11 +7,11 @@ import "dotenv/config";
 
 const app = express();
 const port = process.env.PORT;
-app.use(cors({ credentials: true }));
 
-app.use(compression());
-app.use(cookieParser());
+app.use(cors({ credentials: true }));
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use("/uploads", express.static("uploads"));
 
 connect();
 
