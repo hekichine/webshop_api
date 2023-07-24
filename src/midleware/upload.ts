3de917a6f7a62fafa578ce52b75c0d6008eca05a
hekichien <chienlx1005@gmail.com>
2023-07-24 14:38:16 +0700

    u

---
+++ src/midleware/upload.ts
@@ -0,0 +1,26 @@
+import multer from "multer";
+
+const FILE_TYPE_MAP: {} = {
+  "image/png": "png",
+  "image/jpg": "jpg",
+  "image/jpeg": "jpeg",
+};
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    const isvalid = FILE_TYPE_MAP[file.mimetype];
+    let uploadError = new Error("invalid image type");
+    if (isvalid) {
+      uploadError = null;
+    }
+    cb(uploadError, "uploads");
+  },
+  filename: function (req, file, cb) {
+    const fileName = file.originalname.split(" ").join("-");
+    const extension = FILE_TYPE_MAP[file.mimetype];
+    cb(null, `${fileName}-${Date.now()}.${extension}`);
+  },
+});
+
+const upload = multer({ storage: storage });
+
+export default upload;
